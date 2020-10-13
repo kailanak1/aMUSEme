370fb7990251627afc82d9ab86fb20ec34eb9ad1
kailana <kailanakahawaii@gmail.com>
2020-10-12 17:05:17 -0700

    make draggable/droppable

--- src/components/PlotOrganizer.js
+++ src/components/PlotOrganizer.js
@@ -5,23 +5,28 @@ import Faker from 'faker';
 
 const plotSections = [
     {
-      id: 'exposistion',
+        id: '1',
+      name: 'exposistion',
       desc: Faker.lorem.sentences()
     },
     {
-      id: 'rising action',
+        id: '2',
+      name: 'rising action',
       desc: Faker.lorem.sentences()
     },
     {
-      id: 'climax',
+        id: '3',
+      name: 'climax',
       desc: Faker.lorem.sentences()
     },
     {
-      id: 'falling action',
+        id: '4',
+      name: 'falling action',
       desc: Faker.lorem.sentences()
     },
     {
-      id: 'resolution',
+        id: '5',
+      name: 'resolution',
       desc: Faker.lorem.sentences()
     }
   ]
@@ -30,10 +35,11 @@ function PlotOrganizer(){
     const [plot, updateSection] = useState(plotSections);
 
     function handleOnDragEnd(result) {
+        console.log(result)
         if (!result.destination) return;
         const items = Array.from(plot);
-        const [reorderedItem] = items.splice(result.source, 1);
-        items.splice(result.destination, 0, reorderedItem);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
 
         updateSection(items);
     }
@@ -42,22 +48,23 @@ function PlotOrganizer(){
         <div>
           <header>
             <h1>Plot Organizer</h1>
+            </header>
             <DragDropContext onDragEnd={handleOnDragEnd}>
                 <Droppable droppableId="plotSections">
                     {(provided) => (
                     <ul className="plotSections" {...provided.droppableProps} ref={provided.innerRef}>
-                    {plot.map(({id, desc}) => {
+                    {plot.map(({name, desc, id}, index) => {
                         return (
-                            <Draggable key={id} draggableId={id} >
+                            <Draggable index={index} key={id} draggableId={id} >
                                 {(provided) => (
-                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                            <div className="plotSections">
-                            <b>{id}</b>
-                            </div>
-                            <p>
-                            <p>{desc}</p>
-                            </p>
-                        </li>
+                                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                    <div className="plotSections">
+                                        <b>{name}</b>
+                                    </div>
+                                    
+                                    <p>{desc}</p>
+                                    
+                                </div>
                         )}
                         </Draggable>
                         );
@@ -67,7 +74,6 @@ function PlotOrganizer(){
                     )}
                 </Droppable>
             </DragDropContext>
-          </header>
         </div>
       );
 }
