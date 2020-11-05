2d9d0b7d2aca05ad5637307b313aa0bc31ed636a
kailana <kailanakahawaii@gmail.com>
2020-11-05 11:22:34 -0800

    add countdown timer

--- src/components/ChallengeForm.js
+++ src/components/ChallengeForm.js
@@ -5,6 +5,10 @@ import styled from 'styled-components'
 const Form = styled.form`
     width: 100%;
 `
+const Title = styled.h3`
+    padding: 8px; 
+
+`
 
 const TextField = styled.textarea`
     display: block;
@@ -17,12 +21,45 @@ const TextField = styled.textarea`
 `
 
 class ChallengeForm extends React.Component{
-  
+    state={
+        minutes: 5, 
+        seconds: 0
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+          const { seconds, minutes } = this.state    
+          if (seconds > 0) {
+            this.setState(({ seconds }) => ({
+              seconds: seconds - 1
+            }))
+          }    if (seconds === 0) {
+            if (minutes === 0) {
+              clearInterval(this.myInterval)
+            } else {
+              this.setState(({ minutes }) => ({
+                minutes: minutes - 1,
+                seconds: 59
+              }))
+            }
+          }
+        }, 1000)
+      }
+
+      componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
   
 
     render(){
+        const { minutes, seconds } = this.state
         return(
            <Form>
+               <Title>   
+                   { minutes === 0 && seconds === 0
+                    ? <h1>Time!</h1>
+                    : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }</Title>
                <TextField/>
            </Form>
         )
