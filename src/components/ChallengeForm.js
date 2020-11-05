8785261c5f3193b0c79df3016f755c45efe0294d
kailana <kailanakahawaii@gmail.com>
2020-11-05 12:03:01 -0800

    add analyze button

--- src/components/ChallengeForm.js
+++ src/components/ChallengeForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 const Form = styled.form`
     width: 100%;
@@ -20,10 +20,32 @@ const TextField = styled.textarea`
     margin: 8px;
 `
 
+function blinkingEffect() {
+    return keyframes`
+      50% {
+        opacity: 0;
+      }
+    `;
+  }
+
+
+
+const TimesUp = styled.text`
+    color: red;
+    animation: ${blinkingEffect} 1s linear infinite;
+`
+
+const AnalysisButton = styled.button`
+    padding: 8px; 
+    margin: 8px;
+`
+
 class ChallengeForm extends React.Component{
     state={
-        minutes: 5, 
-        seconds: 0
+        minutes: 1, 
+        seconds: 0,
+        words: '',
+        analyze: false
     }
 
     componentDidMount() {
@@ -46,22 +68,50 @@ class ChallengeForm extends React.Component{
         }, 1000)
       }
 
-      componentWillUnmount() {
+    componentWillUnmount() {
         clearInterval(this.myInterval)
     }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    analyze = () => {
+        this.setState({
+            analyze: true
+        })
+    }
   
+    countWords = () => {
+        let str = this.state.words
+            var matches = str.match(/[\w\d\’\'-]+/gi);
+            return matches ? matches.length : 0;
+    }
+    
 
     render(){
+        console.log(this.state.words)
         const { minutes, seconds } = this.state
         return(
-           <Form>
-               <Title>   
-                   { minutes === 0 && seconds === 0
-                    ? <h1>Time!</h1>
-                    : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
-                }</Title>
-               <TextField/>
-           </Form>
+            <div>
+            <Form>
+                <Title>   
+                    { minutes === 0 && seconds === 0
+                        ? <TimesUp>Time!</TimesUp>
+                        : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                    }</Title>
+                <TextField
+                value={this.state.words}
+                onChange={this.handleChange}
+                placeholder="Write your story"
+                name="words" 
+                />
+            </Form>
+            <AnalysisButton onClick={this.analyze}>Analyze!</AnalysisButton>
+                    {this.state.analyze ? <p>{`You wrote ${this.countWords()} words and ${this.state.words.length} characters in ____ minutes and ___ seconds.`}</p> : null}
+           </div>
         )
     }
 }
